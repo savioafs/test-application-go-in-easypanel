3f1c7bafe0ba5947793a903fe1cb9512d720aab2
Sávio Alexandre <savioalefreitas@gmail.com>
2025-11-17 22:39:22 -0300

    create route client

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/savioafs/test-application-go-in-easypanel/internal/controller"
 	"github.com/savioafs/test-application-go-in-easypanel/internal/database"
 	"github.com/savioafs/test-application-go-in-easypanel/internal/entity"
 )
@@ -18,23 +18,7 @@ func main() {
 
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-
-	r.GET("/route-one", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "rota 01",
-		})
-	})
-
-	r.GET("/route-two", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "rota 02",
-		})
-	})
+	r.POST("/client", controller.CreateClient)
 
 	port := os.Getenv("PORT")
 	if port == "" {
